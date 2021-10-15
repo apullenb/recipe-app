8d01e887955b5b4e641a6f94a0f1e2a086947061
Andrea Pullen <apullenb@gmail.com>
2021-10-15 14:05:32 -0500

    configure fetch

--- config.js
+++
@@ -1,7 +0,0 @@
-
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-    APP_ID: process.env.APP_ID,
-    APP_KEY: process.env.APP_KEY
-}
---
+++ src/config.js
@@ -0,0 +1,8 @@
+
+
+const config = {
+    REACT_APP_ID: process.env.REACT_APP_ID,
+    REACT_APP_KEY: process.env.REACT_APP_KEY
+}
+
+export default config;
