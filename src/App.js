f827fcd9242ef69f72e0d4d5900d91b3617c1c71
Andrea Pullen <apullenb@gmail.com>
2021-10-15 12:23:55 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const APP_ID = '15605a82';
+  const APP_KEY = '55f3ca6fa96881d7cb186a89f8e71d41'
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Recipe App</h1>
     </div>
   );
 }
